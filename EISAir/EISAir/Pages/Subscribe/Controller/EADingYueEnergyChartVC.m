b16e312f1815fe5edaa0a61efffbb5765613d115
DoubleHH <huanghui@iwaimai.baidu.com>
2017-09-17 21:01:19 +0800

    echart 调试

--- EISAir/EISAir/Pages/Subscribe/Controller/EADingYueEnergyChartVC.m
+++ EISAir/EISAir/Pages/Subscribe/Controller/EADingYueEnergyChartVC.m
@@ -14,14 +14,53 @@
     UIView *_headerView;
     UIView *_detailView;
     PYEchartsView *_echartsView;
+    
+    NSArray *_dataArray;
 }
 
 @end
 
 @implementation EADingYueEnergyChartVC
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)initData {
+    [super initData];
+    float totalWidth = SCREEN_WIDTH * 0.55;
+    float max = 3000;
+    _dataArray =
+    @[
+      @{
+          @"title": @"高压进线段一",
+          @"detail": @"3000kwh",
+          @"count":@(3000),
+          @"width": @((int)(3000.0 / max * totalWidth)),
+          @"color": HexColor(0x28cfc1),
+          },
+      @{
+          @"title": @"高压进线段2",
+          @"detail": @"3000kwh",
+          @"count": @(2100),
+          @"width": @((int)(2100.0 / max * totalWidth)),
+          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.8],
+          },
+      @{
+          @"title": @"高压进线段3",
+          @"detail": @"3000kwh",
+          @"count":@(3000),
+          @"width": @((int)(1500.0 / max * totalWidth)),
+          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.7],
+          },
+      @{
+          @"title": @"高压进线段4",
+          @"detail": @"1000kwh",
+          @"count": @(1000),
+          @"width": @((int)(1000.0 / max * totalWidth)),
+          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.5],
+          },
+      ];
+}
+
+- (void)initView {
+    [super initView];
     self.title = @"支路用电";
     self.view.backgroundColor = [UIColor whiteColor];
     
@@ -37,13 +76,13 @@
 }
 
 - (void)addHeader {
-    _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, FlexibleHeightTo6(240) + 67)];
+    _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, FlexibleHeightTo6(240) + 62)];
     _headerView.backgroundColor = [UIColor whiteColor];
     [_contentView addSubview:_headerView];
     
     UILabel *titleLabel = TKTemplateLabel2([UIFont systemFontOfSize:15], HexColor(0x444444), @"一级");
     titleLabel.left = 15;
-    titleLabel.top = 18;
+    titleLabel.top = 20;
     [_headerView addSubview:titleLabel];
     
     UILabel *detailLabel = TKTemplateLabel2([UIFont systemFontOfSize:15], HexColor(0x9b9b9b), @"查看详情");
@@ -55,23 +94,32 @@
     [detailControl addTarget:self action:@selector(detailClick) forControlEvents:UIControlEventTouchUpInside];
     [_headerView addSubview:detailControl];
     
-    _echartsView = [[PYEchartsView alloc] initWithFrame:CGRectMake(15, 52, SCREEN_WIDTH - 30, FlexibleHeightTo6(240))];
+    _echartsView = [[PYEchartsView alloc] initWithFrame:CGRectMake(15, 50, SCREEN_WIDTH - 30, FlexibleHeightTo6(240))];
     [_echartsView setOption:[self echartData]];
     [_echartsView loadEcharts];
     [_headerView addSubview:_echartsView];
 }
 
 - (PYOption *)echartData {
-    NSArray *dataArray = @[
-                           @{@"name":@"三星",
-                             @"value":@3000},
-                           @{@"name":@"小米",
-                             @"value":@2100},
-                           @{@"name":@"苹果",
-                             @"value":@1500},
-                           @{@"name":@"华为",
-                             @"value":@1000},
-                           ];
+    NSMutableArray *dataArray = [NSMutableArray array];
+    for (NSDictionary *dict in _dataArray) {
+        [dataArray addObject:[PYTreeSeriesData initPYTreeSeriesDataWithBlock:^(PYTreeSeriesData *data) {
+            data
+            .nameEqual(dict[@"title"])
+            .valueEqual(dict[@"count"])
+            .itemStyleEqual([PYItemStyle initPYItemStyleWithBlock:^(PYItemStyle *itemStyle) {
+                itemStyle.normalEqual([PYItemStyleProp initPYItemStylePropWithBlock:^(PYItemStyleProp *normal) {
+                    normal.colorEqual([[PYColor alloc] initWithColor:dict[@"color"]])
+                    .labelEqual([PYLabel initPYLabelWithBlock:^(PYLabel *label) {
+                        label.showEqual(NO);
+                    }])
+                    .borderWidthEqual(@4)
+                    .borderColorEqual(PYRGBA(255, 255, 255, 1))
+                    ;
+                }]);
+            }]);
+        }]];
+    }
     return [PYOption initPYOptionWithBlock:^(PYOption *option) {
         option.tooltipEqual([PYTooltip initPYTooltipWithBlock:^(PYTooltip *tooltip) {
             tooltip.triggerEqual(PYTooltipTriggerItem)
@@ -83,49 +131,9 @@
             .sizeEqual(@[@"100%", @"100%"])
             .nameEqual(@"")
             .typeEqual(PYSeriesTypeTreemap)
-            .itemStyleEqual([PYItemStyle initPYItemStyleWithBlock:^(PYItemStyle *itemStyle) {
-                itemStyle.normalEqual([PYItemStyleProp initPYItemStylePropWithBlock:^(PYItemStyleProp *normal) {
-                    normal.labelEqual([PYLabel initPYLabelWithBlock:^(PYLabel *label) {
-                        label.showEqual(NO);
-                    }])
-                    .borderWidthEqual(@0);
-                }]);
-            }])
             .addDataArr(dataArray);
         }]);
     }];
-}
-
-- (NSArray *)datas {
-    float totalWidth = SCREEN_WIDTH * 0.55;
-    float max = 3000;
-    return
-    @[
-      @{
-          @"title": @"高压进线段一",
-          @"detail": @"3000kwh",
-          @"width": @((int)(3000.0 / max * totalWidth)),
-          @"color": HexColor(0x28cfc1),
-          },
-      @{
-          @"title": @"高压进线段2",
-          @"detail": @"3000kwh",
-          @"width": @((int)(2100.0 / max * totalWidth)),
-          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.8],
-          },
-      @{
-          @"title": @"高压进线段3",
-          @"detail": @"3000kwh",
-          @"width": @((int)(1500.0 / max * totalWidth)),
-          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.7],
-          },
-      @{
-          @"title": @"高压进线段4",
-          @"detail": @"3000kwh",
-          @"width": @((int)(1000.0 / max * totalWidth)),
-          @"color": [HexColor(0x28cfc1) colorWithAlphaComponent:0.5],
-          },
-      ];
 }
 
 - (void)addDetail {
@@ -134,7 +142,7 @@
     [_contentView addSubview:_detailView];
     
     float top = 0;
-    NSArray *datas = [self datas];
+    NSArray *datas = _dataArray;
     for (int i = 0; i < datas.count; ++i) {
         NSDictionary *dict = datas[i];
         EAEnergyCountControl *control = [[EAEnergyCountControl alloc] initWithFrame:CGRectMake(0, top, SCREEN_WIDTH, 50)];
