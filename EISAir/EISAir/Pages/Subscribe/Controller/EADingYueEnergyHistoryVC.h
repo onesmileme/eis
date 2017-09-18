578923f65d3f0854da8cbb863f72cb2861388dd4
DoubleH <huanghui2008123@163.com>
2017-09-18 23:56:37 +0800

    订阅历史

---
+++ EISAir/EISAir/Pages/Subscribe/Controller/EADingYueEnergyHistoryVC.h
@@ -0,0 +1,13 @@
+//
+//  EADingYueEnergyHistoryVC.h
+//  EISAir
+//
+//  Created by iwm on 2017/9/18.
+//  Copyright © 2017年 onesmile. All rights reserved.
+//
+
+#import "EABaseViewController.h"
+
+@interface EADingYueEnergyHistoryVC : EABaseViewController
+
+@end
---
+++ EISAir/EISAir/Pages/Subscribe/Controller/EADingYueEnergyHistoryVC.m
@@ -0,0 +1,25 @@
+//
+//  EADingYueEnergyHistoryVC.m
+//  EISAir
+//
+//  Created by iwm on 2017/9/18.
+//  Copyright © 2017年 onesmile. All rights reserved.
+//
+
+#import "EADingYueEnergyHistoryVC.h"
+
+@interface EADingYueEnergyHistoryVC ()
+
+@end
+
+@implementation EADingYueEnergyHistoryVC
+
+- (void)initData {
+    [super initData];
+}
+
+- (void)initView {
+    [super initView];
+}
+
+@end
